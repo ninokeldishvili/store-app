941afcbd6c90072a9acff80dc4820040badcac2f
ninokeldishvili <Ninokeldishvili@gmail.com>
2021-07-23 19:26:36 +0400

    Apply migrations

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -18,8 +18,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(1)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -70,6 +71,7 @@ class OrdderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
     customer = models.OneToOneField(
         Customer, on_delete=models.CASCADE, primary_key=True)
 
